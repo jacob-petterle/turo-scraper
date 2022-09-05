45a8a3198622ef20954d6c1c3f7f76cfa8619129
Jacob Petterle <jacobpetterle@gmail.com>
2022-09-04 19:33:14 -0600

    add main layout component

--- client/src/providers/AppProvider.tsx
+++ client/src/providers/AppProvider.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 
 function AppProvider() {
+  // TODO: Add a loading component
+  // TODO Add other providers
+  // TODO Add routing
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
-
+      
     </React.Suspense>
   );
 }
