8e5e41d8cba365c83e6238a8196b5dc468320de1
Jacob Petterle <jacobpetterle@gmail.com>
2022-09-08 22:32:48 -0600

    Fix webpack config for alias imports

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import {Col} from "react-bootstrap";
 
-import {getListingCount} from "@features/totals/api/getListingCount";
+import {getListingCount} from "features/totals/api/getListingCount";
 
 
 const Sidebar = () => {
